422311ec95b156be168bfe5ead5502ca43959c19
Andy Baker <andy@andybak.net>
2015-02-03 14:00:39 +0000

    Allow use of a callable to define test recipients via TEST_RECIPIENTS_GETTER. Rename ALSO_BCC to be more explicit

--- debugmail/backends.py
+++ debugmail/backends.py
@@ -1,3 +1,4 @@
+from django.utils.module_loading import import_string
 from .base_settings import *
 import logging
 from django.core.mail.backends.smtp import EmailBackend
@@ -37,10 +38,18 @@ class DebugEmailBackend(EmailBackend):
     def send_messages(self, email_messages):
 
         if SEND_TO_TEST_RECIPIENTS:
-            self._send_to_alternates(
-                email_messages,
-                TEST_RECIPIENTS,
-            )
+
+            if TEST_RECIPIENTS_GETTER is not None:
+                test_recipients = import_string(TEST_RECIPIENTS_GETTER)()
+            else:
+                test_recipients = TEST_RECIPIENTS
+            print "-"*80
+            print test_recipients
+            print "-"*80
+            # self._send_to_alternates(
+            #     email_messages,
+            #     test_recipients,
+            # )
 
         if SEND_TO_ADMINS:
             self._send_to_alternates(
@@ -50,7 +59,7 @@ class DebugEmailBackend(EmailBackend):
 
         if SEND_TO_REAL_RECIPIENTS:
 
-            if ALSO_BCC:
+            if ALSO_BCC_REAL_EMAILS:
                 for email in email_messages:
                     email.bcc += BCC_RECIPIENTS
 
--- debugmail/base_settings.py
+++ debugmail/base_settings.py
@@ -5,7 +5,9 @@ SEND_TO_TEST_RECIPIENTS = getattr(settings, 'MAILER_SEND_TO_TEST_RECIPIENTS', Fa
 SEND_TO_ADMINS = getattr(settings, 'MAILER_SEND_TO_ADMINS', True)
 LOG_TO_DB = getattr(settings, 'MAILER_LOG_TO_DB', True)
 LOG_ALL_TO_DB = getattr(settings, 'MAILER_LOG_ALL_TO_DB', False)
-ALSO_BCC = getattr(settings, 'MAILER_ALSO_BCC', False)
+ALSO_BCC_REAL_EMAILS = getattr(settings, 'MAILER_ALSO_BCC_REAL_EMAILS', False)
 BCC_RECIPIENTS = getattr(settings, 'MAILER_BCC_RECIPIENTS', None)
 TEST_RECIPIENTS = getattr(settings, 'MAILER_TEST_RECIPIENTS', None)
+TEST_RECIPIENTS_GETTER = getattr(settings, 'MAILER_TEST_RECIPIENTS_GETTER', None)
+
 
